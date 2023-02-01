e0955b20ff36a07a90bce61fed3bee7fbe5377cd
Fanixtar <96950043+Fanixtar@users.noreply.github.com>
2023-02-02 00:30:15 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml;
+
+namespace CustomBuildStepsDisabler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var replace = @"<Command>""$(OutDir)injector.exe""
+powershell -nop -c ""&amp; {sleep 15}""</Command>";
+            var path = Path.Combine(Environment.CurrentDirectory, "cheat-library", "cheat-library.vcxproj");
+            var text = File.ReadAllText(path);
+            text = text.Replace(replace, "<Command></Command>");
+            File.WriteAllText(path, text);
+        }
+    }
+}
